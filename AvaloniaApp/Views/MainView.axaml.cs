76aed36727396c08dc589cf0c30951f1f0f4b2d2
Alex.Bob <billbob.dong@gmail.com>
2024-11-29 12:05:28 +0800

    🎨 style(`AvaloniaApp/Views/MainWindow.axaml`): Fix whitespace and format issues in XAML.

--- AvaloniaApp/Views/MainView.axaml.cs
+++ AvaloniaApp/Views/MainView.axaml.cs
@@ -4,6 +4,8 @@ namespace AvaloniaApp.Views;
 
 public partial class MainView : UserControl
 {
+    public string Greeting => "sfasfdsafsdaf";
+
     public MainView()
     {
         InitializeComponent();
