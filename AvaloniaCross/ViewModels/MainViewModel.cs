b0f926737ab30a4fc00ad058525389f24811ba10
Alex.Bob <billbob.dong@gmail.com>
2024-12-02 15:13:34 +0800

    ✨ feat(`ViewModels/MainViewModel.cs`, `ViewModels/ViewModelBase.cs`): Add user authentication properties and reactive properties.

--- AvaloniaCross/ViewModels/MainViewModel.cs
+++ AvaloniaCross/ViewModels/MainViewModel.cs
@@ -1,14 +1,16 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using ReactiveUI;
-using ReactiveUI.SourceGenerators;
+﻿using ReactiveUI.SourceGenerators;
 
 namespace AvaloniaCross.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    [ObservableAsProperty]
-    private string _userName = string.Empty;
-    
-    [ObservableAsProperty]
-    private string _greeting = "Welcome to Avalonia!";
+    [ObservableAsProperty] private string _userName = string.Empty;
+
+    [ObservableAsProperty] private string _greeting = "Welcome to Avalonia!";
+
+    [ObservableAsProperty] private string _inputText = string.Empty;
+
+    [ObservableAsProperty] private string _username = string.Empty;
+
+    [Reactive] private string _password = string.Empty;
 }
